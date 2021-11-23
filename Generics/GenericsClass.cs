c2bdff9e05aae9360d025db4cb76aa6ea3792d37
“Arunesh <as8175802986@gmail.com>
2021-11-24 00:52:27 +0530

    [Arunrsh] Added code for maximum

---
+++ Generics/GenericsClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class GenericsClass
+    {
+        public static int MaxNumber(int firstNum, int secondNum, int thirdNum)
+        {
+            if ((firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0) || (firstNum.CompareTo(secondNum) >= 0 && firstNum.CompareTo(thirdNum) > 0) ||
+                    (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) >= 0))
+            {
+                return firstNum;
+
+            }
+            else if ((secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0) || (secondNum.CompareTo(firstNum) >= 0 && secondNum.CompareTo(thirdNum) > 0) ||
+                (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) >= 0))
+            {
+                return secondNum;
+            }
+            else
+            {
+                return thirdNum;
+            }
+        }
+    }
+}

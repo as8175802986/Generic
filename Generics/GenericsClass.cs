974f21f4c8ce8a5e401b7bec2d32d76047c3d42e
Arunesh sharma <73111126+as8175802986@users.noreply.github.com>
2021-11-24 06:41:16 +0530

    Update GenericsClass.cs

--- Generics/GenericsClass.cs
+++ Generics/GenericsClass.cs
@@ -6,17 +6,28 @@ using System.Threading.Tasks;
 
 namespace Generics
 {
-    class GenericsClass
+    
+       class GenericsClass<T> where T: IComparable                   //Tcomparable is used to identifu the compare to class method
     {
-        public static int MaxNumber(int firstNum, int secondNum, int thirdNum)
+        private T firstNum, secondNum, thirdNum;                //declaring instance variables
+
+        public GenericClass(T firstNum, T secondNum, T thirdNum)       //declaring the class consstructor
         {
+            this.firstNum = firstNum;
+            this.secondNum = secondNum;
+            this.thirdNum = thirdNum;
+        }
+        public static T MaxOf(T firstNum, T secondNum,T thirdNum)              //declaring the max of method. statci so that we can use it without creating an objecct in this class
+        {
+            //comparision of three values
+
             if ((firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0) || (firstNum.CompareTo(secondNum) >= 0 && firstNum.CompareTo(thirdNum) > 0) ||
                     (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) >= 0))
             {
                 return firstNum;
 
             }
-            else if ((secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0) || (secondNum.CompareTo(firstNum) >= 0 && secondNum.CompareTo(thirdNum) > 0) ||
+            else if ((secondNum.CompareTo(firstNum)>0 && secondNum.CompareTo(thirdNum)>0) || (secondNum.CompareTo(firstNum) >= 0 && secondNum.CompareTo(thirdNum) > 0) ||
                 (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) >= 0))
             {
                 return secondNum;
@@ -24,7 +35,14 @@ namespace Generics
             else
             {
                 return thirdNum;
-            }
+            }          
+        }
+
+        public T MaxValue()                        //another method that applies the constructor and thenpronts or returns the value
+        {
+            T max = GenericClass<T>.MaxOf(this.firstNum, this.secondNum, this.thirdNum);
+            return max;
         }
     }
+    
 }

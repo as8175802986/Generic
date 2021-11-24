ceffc57390c344fcfe1f1da29aaa63ea5d44fc8e
Arunesh sharma <73111126+as8175802986@users.noreply.github.com>
2021-11-24 06:35:43 +0530

    Update Program.cs

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -10,7 +10,16 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GenericsClass.MaxNumber(100, 30, 30));
+                  //calling the generic class method by creating different objects
+            //we need to create diff objects so that we can use other data types 
+
+           GenericClass<int> myObj= new GenericClass<int>(20, 35,55);
+            GenericClass<double> myObj1 = new GenericClass<double>(55.265, 35.0, 55.256);
+            GenericClass<string> myObj2 = new GenericClass<string>("abc","xyz","pqr");
+
+            Console.WriteLine(myObj.MaxValue());
+            Console.WriteLine(myObj1.MaxValue());
+            Console.WriteLine(myObj2.MaxValue());
         }
     }
 }
